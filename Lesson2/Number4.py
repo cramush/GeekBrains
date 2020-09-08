3d784553d006eca016434214fcea80969c39f88c
cramush <cramush@gmail.com>
2020-09-08 20:53:12 +0300

    Доделал задания ко второму уроку.

--- Lesson2/Number4.py
+++ Lesson2/Number4.py
@@ -1,15 +1,8 @@
 line = str(input("Введи строку из нескольких слов через пробел: "))
-word = []
 number = 0
+words = line.split(" ")
 
-for i in range(line.count(' ') + 1):
-    word = line.split()
-    if len(str(word)) <= 10:
-        number += 1
-        print(f"{number} {word[i]}")
-    else:
-        number += 1
-        print(f"{number} {word[i] [0:10]}")
-
-
+for word in words:
+    number += 1
+    print(f"{number} {word [0:10]}")
 
